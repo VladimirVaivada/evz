6d89ccd555ddc51d3b1ea3066573da13916fa7d8
VladimirVaivada <vaivadavladimir@gmail.com>
2020-05-12 17:12:34 +0300

    Дополняет ссылки на телефон и почту нужными атрибутами

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,6 +39,8 @@
         <v-divider />
         <v-list-item
           class="my-1"
+          target="_blank"
+          rel="noreferrer noopener"
           href="mailto:chained_@mail.ru?subject=%D0%92%D0%BE%D0%BF%D1%80%D0%BE%D1%81%20%D0%BD%D0%B0%20%D1%81%D0%B0%D0%B9%D1%82%D0%B5%3A%20%28%20%D0%A2%D0%B5%D0%BC%D0%B0%20%D0%92%D0%B0%D1%88%D0%B5%D0%B3%D0%BE%20%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%B0%20%29"
         >
           <v-list-item-action>
@@ -48,7 +50,12 @@
             Написать
           </v-list-item-content>
         </v-list-item>
-        <v-list-item href="tel:+79119644740" class="my-1">
+        <v-list-item
+          target="_blank"
+          rel="noreferrer noopener"
+          href="tel:+79119644740"
+          class="my-1"
+        >
           <v-list-item-action>
             <v-icon color="red">mdi-phone</v-icon>
           </v-list-item-action>
