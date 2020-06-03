4f466b083e9b6fe9afecfc2b9657177fe7474c80
VladimirVaivada <vaivadavladimir@gmail.com>
2020-06-03 14:17:43 +0300

    Добавляет favicon.ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
       {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"},
       {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
       {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
+      {rel:"icon", type:"image/x-icon", href: "/favicon.ico"},
       {rel:"manifest", href:"/site.webmanifest"},
       {rel:"mask-icon", href:"/safari-pinned-tab.svg", color:"#844dc9"},
       {name:"msapplication-TileColor", content:"#da532c"},
