9e0dcae179ae8fd1f0644b2ffcba9d483d3d435d
VladimirVaivada <vaivadavladimir@gmail.com>
2020-06-13 17:13:02 +0300

    Фиксит ссылку на шрифт

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       }
     ],
     link: [
-      {rel:"preload", as:"font", type:"font/woff2", href:"fonts/Roboto-Regular-firstStage.woff2"},
+      {rel:"preload", as:"font", type:"font/woff2", href:"/fonts/Roboto-Regular-firstStage.woff2"},
       {rel:"apple-touch-icon", sizes:"180x180", href:"/apple-touch-icon.png"},
       {rel:"icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
       {rel:"icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
